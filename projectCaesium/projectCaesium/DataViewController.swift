34986d7496d5433566f0dda854eeb427b8a4110f
Stephen2697 <c16377163@mydit.ie>
2018-04-13 15:42:46 +0100

    Commit Two: Formation

---
+++ projectCaesium/projectCaesium/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  projectCaesium
+//
+//  Created by Stephen Alger on 13/04/2018.
+//  Copyright © 2018 Stephen Alger. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+

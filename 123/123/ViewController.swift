f52b9c0ce08d1659096df54188e090cd2a1cab5a
Stephen2697 <c16377163@mydit.ie>
2018-04-16 14:10:56 +0100

    Storyboard Layouts and Initial ViewController.swift code

---
+++ 123/123/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  123
+//
+//  Created by Stephen Alger on 13/04/2018.
+//  Copyright © 2018 Stephen Alger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

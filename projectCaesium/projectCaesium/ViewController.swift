eca608eb6e0b923b68ef82100d57a0c0058de624
Stephen2697 <c16377163@mydit.ie>
2018-04-19 23:22:55 +0100

    View Controller Error Fixing - Functioning When Tested
    Albeit Minimal functionality - building blocks in place.

--- projectCaesium/projectCaesium/ViewController.swift
+++ projectCaesium/projectCaesium/ViewController.swift
@@ -8,71 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController
-{
-    
-//class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     //MARK: Properties
-//    @IBOutlet weak var nameFoodItem: UITextField!
-//    @IBOutlet weak var dsiplayedFoodItem: UILabel!
-    
-    //bug fixing variables
 
     
-    @IBAction func hi(_ sender: Any)
-    {
-        print("hello Stephen")
-    }
+    @IBOutlet weak var input: UITextField!
+    @IBOutlet weak var output: UILabel!
     
-    
-//    @IBOutlet weak var dispName: UILabel!
-//    @IBOutlet weak var enterName: UITextField!
-    
-    override func viewDidLoad()
-    {
+    override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Handle the text field’s user input through delegate callbacks.
-//        nameFoodItem.delegate = self
-//        enterName.delegate = self
+        
+        input.delegate = self
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    
     
     //MARK: UITextFieldDelegate
     //Using a delegate to handle textfield interaction
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
+        textField.resignFirstResponder()
+        return true
+    }
     
-    //Handle event when User has finished editing textfield
-    //Called by system when Return is entered on Keyboard
-//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-//
-//        //Resign textField's first responder status...
-//        //This acts to Hide the keyboard
-//        textField.resignFirstResponder()
-//
-//        //True - we want to process enter data after the return key being pressed
-//        return true
-//    }
-
-//    func textFieldDidEndEditing(_ textField: UITextField) {
-//
-////        dsiplayedFoodItem.text = textField.text
-//
-//        dispName.text = textField.text
-//    }
-
-    
-    
-    //MARK: Actions
-    
-//    @IBAction func addFoodItem(_ sender: UIButton) {
-//    }
+    func textFieldDidEndEditing(_ textField: UITextField)
+    {
+        output.text = textField.text
+    }
     
 }
 

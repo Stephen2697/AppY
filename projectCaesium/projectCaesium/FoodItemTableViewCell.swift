fe262d7be0137c1046c9f3e37b4a78ed59bc8ce3
Stephen2697 <c16377163@mydit.ie>
2018-04-23 14:11:57 +0100

    FoodItemTableViewCell Class Added
    Creating Custom Table Cell Class tio reporesent our custom designed table view cells.

---
+++ projectCaesium/projectCaesium/FoodItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FoodItemTableViewCell.swift
+//  123
+//
+//  Created by Stephen Alger on 23/04/2018.
+//  Copyright © 2018 Stephen Alger. All rights reserved.
+//
+
+import UIKit
+
+class FoodItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

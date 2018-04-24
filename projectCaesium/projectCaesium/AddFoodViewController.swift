fe5273b205ced2b2fbd1a9e1bbc1ff4a35ba927c
Stephen2697 <c16377163@mydit.ie>
2018-04-24 12:49:01 +0100

    Added Code to dismiss the keyboard upon user pressing 'Done' on keyboard or selecting anywhere outside the textField

--- projectCaesium/projectCaesium/AddFoodViewController.swift
+++ projectCaesium/projectCaesium/AddFoodViewController.swift
@@ -11,31 +11,46 @@ import UIKit
 class AddFoodViewController: UIViewController, UITextFieldDelegate {
 
     //MARK: Properties
-
     
-    
-
-    
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var mealNameBox: UITextField!
+    @IBOutlet weak var caloriesBox: UITextField!
+    @IBOutlet weak var portionBox: UITextField!
+    @IBOutlet weak var proteinBox: UITextField!
+    @IBOutlet weak var carbBox: UITextField!
+    @IBOutlet weak var fatBox: UITextField!
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
-        //input.delegate = self
+        self.mealNameBox.delegate = self
+        self.caloriesBox.delegate = self
+        self.portionBox.delegate = self
+        self.proteinBox.delegate = self
+        self.carbBox.delegate = self
+        self.fatBox.delegate = self
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
     //MARK: UITextFieldDelegate
     //Using a delegate to handle textfield interaction
-//    func textFieldShouldReturn(_ textField: UITextField) -> Bool
-//    {
-//        textField.resignFirstResponder()
-//        return true
-//    }
-//
-//    func textFieldDidEndEditing(_ textField: UITextField)
-//    {
-//        output.text = textField.text
-//    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
+        mealNameBox.resignFirstResponder()
+        caloriesBox.resignFirstResponder()
+        carbBox.resignFirstResponder()
+        fatBox.resignFirstResponder()
+        proteinBox.resignFirstResponder()
+        portionBox.resignFirstResponder()
+        
+        return true
+    }
+
     
 }
 

122537db67c9a9e920c4316b41f8005e16b07161
Stephen2697 <c16377163@mydit.ie>
2018-04-26 14:40:03 +0100

    preferLargeTitles Added Universally

---
+++ projectCaesium/projectCaesium/DevViewController.swift
@@ -0,0 +1,41 @@
+//
+//  DevViewController.swift
+//  123
+//
+//  Created by Stephen Alger on 26/04/2018.
+//  Copyright © 2018 Stephen Alger. All rights reserved.
+//
+
+import UIKit
+
+class DevViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavBar()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //configure ios11 style nav bar with preferLargeTitles boolean attribute setu to true
+    func setupNavBar()
+    {
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
